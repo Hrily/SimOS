124722687c5a0258413cabf0219dd98c6ee64c78
Prerana97 <prerana12897@gmail.com>
2017-04-04 10:57:38 -0700

    FIFO

--- page_replacement/fifo.c
+++ page_replacement/fifo.c
@@ -4,26 +4,26 @@ int main()
 {
         int i,j,n,a[50],frame[10],no,k,avail,count=0;
         float hr,fra;
-        printf("Enter the number of frames: ");
+       // printf("Enter the number of frames: ");
         scanf("%d",&no);
-        printf("Enter the number of pages: ");
+       // printf("Enter the number of pages: ");
         scanf("%d",&n);
-        printf("Enter the page reference string: ");
+       // printf("Enter the page reference string: ");
         for(i=1;i<=n;i++)
         scanf("%d",&a[i]);
         for(i=0;i<no;i++)
         frame[i]= -1;
     j=0;
-    printf("\tRef string\t page frames\n");
+   // printf("\tRef string\t page frames\n");
         for(i=1;i<=n;i++)
     	{
-        printf("%d\t\t",a[i]);
+       // printf("%d\t\t",a[i]);
         avail=0;
         for(k=0;k<no;k++)
                 if(frame[k]==a[i])
                 {
-                        for(k=0;k<no;k++)
-                                printf("%d\t",frame[k]);
+                //        for(k=0;k<no;k++)
+         //                       printf("%d\t",frame[k]);
                                 avail=1;
                 }
                 if (avail==0)
@@ -32,16 +32,16 @@ int main()
                                 frame[j]=a[i];
                                 j=(j+1)%no;
                                 count++;
-                                for(k=0;k<no;k++)
-                                        printf("%d\t",frame[k]);
+            //                    for(k=0;k<no;k++)
+           //                             printf("%d\t",frame[k]);
                 }
-                printf("\n");
+              //  printf("\n");
 
    	}
         fra=(float)count/n;
         hr=1-fra;
        // printf("\nPage fault is %d\n",count);
-        printf("Hit ratio is %f\n",hr);
+        printf("%f\n",hr);
       //  printf("Fault ratio is %f\n",fra);
         return 0;
 }
